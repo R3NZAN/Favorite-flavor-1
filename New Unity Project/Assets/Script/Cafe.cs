91d336f80e1ffc9b1a1a03d3281291ae23565b2c
JosephOne1 <lionbrotherhood.arg@gmail.com>
2021-04-27 04:17:50 -0300

    Código de los alimentos y puntuacion
    Hice el código de la creación de bebidas (Café, Te, Agua, etc.) y de la puntuación, ahora se pueden hacer cualquier bebida del drive que armamos y te la puntúan

---
+++ New Unity Project/Assets/Script/Cafe.cs
@@ -0,0 +1,116 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+[System.Serializable]
+public class Ingredientes
+{
+    public string name;
+    public int cuanto;
+    public Color color;
+}
+
+public class Cafe : MonoBehaviour
+{
+    public float nota;
+
+    [Space(10)]
+
+    public float putuacion;
+    public float bebidaTotal;
+    public bool evaluar;
+
+    [Space(20)]
+
+    public int suma;
+    public Ingredientes[] ingrediente;
+    public Image[] imageIngrediente;
+    public TextMeshProUGUI textNota;
+    Color tazaColorOriginal;
+
+    [Space(20)]
+
+    public CafeCreation bebida;
+
+    void Start()
+    {
+        tazaColorOriginal = imageIngrediente[0].color;
+
+        bebida.total = 0;
+        bebida.Suma();
+        bebidaTotal = bebida.total;
+    }
+
+    void Update()
+    {
+        Evaluacion();
+    }
+
+    void Evaluacion()
+    {
+        if(evaluar == true)
+        {
+            for (var i = 0; i < ingrediente.Length; i++)
+            {
+                if (bebida.ingrediente[i].cuanto != 0)
+                {
+                    if (ingrediente[i].cuanto > bebida.ingrediente[i].cuanto)
+                    {
+                        putuacion += bebida.ingrediente[i].cuanto;
+                    }
+                    else
+                    {
+                        putuacion += ingrediente[i].cuanto;
+                    }
+                }
+
+                nota = putuacion / bebidaTotal * 10;
+
+                evaluar = false;
+            }
+        }
+
+        textNota.text = nota.ToString();
+    }
+
+    public void Ingrediente(string ing)
+    {
+        if(suma < 5)
+        {
+            for (var i = 0; i < ingrediente.Length; i++)
+            {
+                if (ingrediente[i].name == ing)
+                {
+                    ingrediente[i].cuanto += 1;
+                    imageIngrediente[suma].color = ingrediente[i].color;
+                    suma += 1;
+                }
+            }
+        }
+
+    }
+
+    public void Resetear()
+    {
+        for (var i = 0; i < ingrediente.Length; i++)
+        {
+            imageIngrediente[0].color = tazaColorOriginal;  
+            imageIngrediente[1].color = tazaColorOriginal;
+            imageIngrediente[2].color = tazaColorOriginal;
+            imageIngrediente[3].color = tazaColorOriginal;
+            imageIngrediente[4].color = tazaColorOriginal;
+
+            nota = 0;
+            putuacion = 0;
+            ingrediente[i].cuanto = 0;
+            suma = 0;
+        }
+    }
+
+    public void Calificar()
+    {
+        evaluar = true;
+    }
+}
---
+++ New Unity Project/Assets/Script/CafeCreation.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Cafe", menuName = "Cafe")]
+public class CafeCreation : ScriptableObject
+{
+    public string cafeName;
+    public int total;
+    public Ingrediente[] ingrediente;
+
+    //public int cafe, agua, espuma, ron, lecheComun, lecheAlmendra, vainilla, azucar, azucarMoreno, matcha;
+
+    public void Suma()
+    {
+        for (var i = 0; i < ingrediente.Length; i++)
+        {
+            total += ingrediente[i].cuanto;
+        }
+    }
+
+    [System.Serializable]
+    public class Ingrediente
+    {
+        public string name;
+        public int cuanto;
+    }
+}
+
+
